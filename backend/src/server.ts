53d62f5f56d1c328c4a4fd78acc446ea563d2db3
Margaret Camilletti <mhcamilletti@gmail.com>
2023-07-28 15:01:58 -0400

    Got login working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -54,7 +54,7 @@ app.get("/api/reviews/:id", (req, res) => {
 
 app.post("/api/users/login", (req, res) => {
     const {email, password} = req.body;
-    const user = sample_users.find((user) => user.email === email && user.password === password);
+    const user = sample_users.find(user => user.email === email && user.password === password);
 
     if(user){
         res.send(generateTokenResponse(user));
