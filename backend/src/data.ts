2d94814bb0d7d3ce9ff6d51603d169c48c7169e4
Margaret Camilletti <mhcamilletti@gmail.com>
2023-11-16 14:09:52 -0500

    added images to backend folder

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -6,7 +6,7 @@ export const candles: any[] = [
         price: 31,
         favorite: false,
         stars: 5,
-        imageUrl: 'https://cloudfront-us-east-1.images.arcpublishing.com/tbt/OUPR73CMB5EM7IJNWUBXTKOMXY.jpeg',
+        imageUrl: './src/assets/images/candle1.jpg',
         tags: ['autumn', 'fresh and clean'],
         scents: ['clove', 'lavender', 'basalm', 'clary sage', 'persimmon', 'rhubarb', 'lavender', 'geranium', 'eucalyptus woods', 'basalm'],
         description: 'Spend the perfect autumn day strolling tree-lined streets with the scents of clove, lavender, balsam, and eucalyptus woods.'
@@ -16,7 +16,7 @@ export const candles: any[] = [
         price: 31,
         favorite: false,
         stars: 4.8,
-        imageUrl: 'https://voltavienna.com/wp-content/uploads/2022/11/Volta_Provider-Store_River-Candle_Hikari2-1024x1412.jpg',
+        imageUrl: './assets/images/candle2.jpg',
         tags: ['autumn', 'woody'],
         scents: ['cardamom', 'black vanilla', 'smoked oud', 'bergamot', 'red cedar', 'birch bark', 'amber', 'suede', 'burnished leather', 'mahogany wood'],
         description: 'Feel the cool autumn breeze on a twilight walk along the river, with the scents of cardamom, black vanilla, and smoked oud.'
@@ -26,7 +26,7 @@ export const candles: any[] = [
         price: 31,
         favorite: false,
         stars: 5,
-        imageUrl: 'https://acultivatednest.com/wp-content/uploads/2016/09/diy-pumpkin-spice-soy-candle-lit.jpg',
+        imageUrl: './assets/images/candle3.jpg',
         tags: ['autumn', 'sweet & spicy'],
         scents: ['cinnamon sugar', 'brown butter', 'spiced pumpkin', 'maple glaze', 'glazed icing', 'caramel', 'spiced croissant dough', 'pumpkin', 'vanilla icing', 'maple'],
         description: 'Enjoy the fragrance of this creamy, delicious treat, with cinnamon sugar, brown butter, spiced pumpkin, and maple glaze.'
@@ -36,7 +36,7 @@ export const candles: any[] = [
         price: 31,
         favorite: false,
         stars: 4.8,
-        imageUrl: 'https://i.etsystatic.com/31522027/r/il/1da297/4748293705/il_fullxfull.4748293705_3814.jpg',
+        imageUrl: './assets/images/candle4.jpg',
         tags: ['autumn', 'fruity'],
         scents: ['mulled apple', 'pear', 'cider spices', 'orange', 'peppercorn', 'cinnamon sugar', 'apple brandy', 'vanilla bean', 'cedar'],
         description: 'Gather family and friends for a toast to autumn with the fragrance of sangria, scented with notes of mulled apple, fresh pear, and cider spices.'
@@ -46,7 +46,7 @@ export const candles: any[] = [
         price: 34,
         favorite: false,
         stars: 4.7,
-        imageUrl: 'https://post.healthline.com/wp-content/uploads/2020/03/burning-candle-matchbook-732x549-thumbnail-732x549.jpg',
+        imageUrl: './assets/images/candle5.webp',
         tags: ['summer', 'citrus'],
         scents: ['sage', 'lemon', 'lime', 'talc', 'lavender', 'geranium', 'patchouli'],
         description: 'This blend of earthy sage, luxurious talc, and lemon-lime creates a natural harmony. '
@@ -56,7 +56,7 @@ export const candles: any[] = [
         price: 13.25,
         favorite: false,
         stars: 4.6,
-        imageUrl: 'https://poetbotanicals.co.nz/cdn/shop/products/BOHOSTOCKPHOTO-1_d4315984-310e-44d4-8947-ea222e7d34a7_grande.jpg?v=1649241883',
+        imageUrl: './assets/images/candle6.webp',
         tags: ['summer', 'woody'],
         scents: ['citrus', 'floral', 'driftwood'],
         description: 'Surround yourself in cozy intrigue — driftwood mingles with brilliant citrus and floral notes for the perfect coastal escape.'
@@ -66,7 +66,7 @@ export const candles: any[] = [
         price: 12,
         favorite: false,
         stars: 4.6,
-        imageUrl: 'https://www.lifenreflection.com/wp-content/uploads/2017/06/INSTA-_apple-candles-11.jpg',
+        imageUrl: './assets/images/candle7.jpg',
         tags: ['winter', 'fruity'],
         scents: ['cinnamon leaf', 'golden apples', 'clove', 'apple peel', 'cracked walnuts', 'brown sugar', 'vanilla', 'amber', 'melted butter', 'maple syrup'],
         description: 'A happy holiday homecoming with the festive aroma of sweet apples, cinnamon, walnuts, and maple. '
@@ -76,7 +76,7 @@ export const candles: any[] = [
         price: 13,
         favorite: false,
         stars: 4.8,
-        imageUrl: 'https://dartez.in/wp-content/uploads/2021/09/DCE54-3.jpeg',
+        imageUrl: './assets/images/candle8.jpg',
         tags: ['spring', 'citrus'],
         scents: ['lemon zest', 'tart lemon', 'vanilla', 'sugar'],
         description: 'So fresh and full of zest, like a bright day exploring hillside villages with sweet unexpected turns.'
@@ -86,7 +86,7 @@ export const candles: any[] = [
         price: 34,
         favorite: false,
         stars: 4.8,
-        imageUrl: 'https://craftsbyamanda.com/wp-content/uploads/2021/04/DIY-lavender-candles-V1-1100x1650.jpg',
+        imageUrl: './assets/images/candle9.jpg',
         tags: ['spring', 'citrus'],
         scents: ['tangerine', 'lemon', 'lavender', 'orange', 'vanilla'],
         description: 'Clean and inviting...a powdery fresh blend of tangy lemon citrus and sweet lavender flowers. '
@@ -96,7 +96,7 @@ export const candles: any[] = [
         price: 16,
         favorite: false,
         stars: 4.7,
-        imageUrl: 'https://www.thegrowingcandle.com/cdn/shop/products/Adelyn.jpg?v=1686949525',
+        imageUrl: './assets/images/candle10.webp',
         tags: ['spring', 'summer', 'floral'],
         scents: ['rose', 'apple', 'citrus', 'geranium', 'musk'],
         description: 'An intoxicating English garden of fragrant heirloom roses. '
