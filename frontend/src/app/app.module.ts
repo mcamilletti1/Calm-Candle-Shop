ef322253168b089eb9366eef5aa21f1461a4c5ed
Margaret Camilletti <mhcamilletti@gmail.com>
2023-11-17 17:08:03 -0500

    added loading gif

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -26,6 +26,7 @@ import { OrderItemsListComponent } from './components/partials/order-items-list/
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
 import { OrderTrackPageComponent } from './components/pages/order-track-page/order-track-page.component';
+import { LoadingComponent } from './components/partials/loading/loading.component';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,8 @@ import { OrderTrackPageComponent } from './components/pages/order-track-page/ord
     CheckoutPageComponent,
     OrderItemsListComponent,
     PaymentPageComponent,
-    OrderTrackPageComponent
+    OrderTrackPageComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
