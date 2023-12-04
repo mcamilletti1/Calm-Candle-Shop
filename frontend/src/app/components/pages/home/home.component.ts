07fd1ce4999546e478b511a28457a71e2828ac02
Margaret Camilletti <mhcamilletti@gmail.com>
2023-12-04 16:58:16 -0500

    updated home component logic

--- frontend/src/app/components/pages/home/home.component.ts
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -6,7 +6,6 @@ import { ReviewService } from 'src/app/services/review.service';
 import { Candle } from 'src/app/shared/models/Candle';
 import { Review } from 'src/app/shared/models/Review'; 
 
-
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -21,44 +20,48 @@ export class HomeComponent implements OnInit {
     private candleService: CandleService,
     private reviewService: ReviewService,
     private activatedRoute: ActivatedRoute
-  ) {
-    let candlesObservable: Observable<Candle[]>;
-    activatedRoute.params.subscribe((params) => {
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.params.subscribe((params) => {
+      let candlesObservable: Observable<Candle[]>;
+
       if (params.searchTerm)
         candlesObservable = this.candleService.getAllCandlesBySearchTerm(params.searchTerm);
       else if (params.tag)
         candlesObservable = this.candleService.getAllCandlesByTag(params.tag);
       else
-        candlesObservable = candleService.getAll();
+        candlesObservable = this.candleService.getAll();
 
       candlesObservable.subscribe((serverCandles) => {
         this.candles = serverCandles;
 
-        for (const candle of serverCandles) {
-          const candleIdAsNumber = parseInt(candle.id, 10);
-          this.reviewService.getReviewsByCandle(candle.id).subscribe(
-            (serverReviews) => {
-              this.reviews = serverReviews;
-              if (serverReviews.length > 0) {
-                let rating = 0;
-                for (const review of serverReviews) {
-                  rating += review.rating;
-                }
-                this.averageRatings[candleIdAsNumber] = rating / serverReviews.length;
-              } else {
-                this.averageRatings[candleIdAsNumber] = 0;
-              }
-            },
-            (error) => {
-              console.error('Error fetching reviews:', error);
-            }
-          );
+        for (const candle of this.candles) {
+          this.calculateAverageRating(candle);
         }
       });
     });
   }
 
-  ngOnInit(): void {
+  private calculateAverageRating(candle: Candle): void {
+    this.reviewService.getReviewsByCandle(candle.id).subscribe(
+      (serverReviews) => {
+        if (serverReviews.length > 0) {
+          let rating = 0;
+          for (const review of serverReviews) {
+            rating += review.rating;
+          }
+          const candleIdAsNumber = parseInt(candle.id, 10);
+          this.averageRatings[candleIdAsNumber] = rating / serverReviews.length;
+        } else {
+          const candleIdAsNumber = parseInt(candle.id, 10);
+          this.averageRatings[candleIdAsNumber] = 0;
+        }
+      },
+      (error) => {
+        console.error('Error fetching reviews:', error);
+      }
+    );
   }
 
   toggleFavorite(candle: Candle): void {
@@ -70,4 +73,5 @@ export class HomeComponent implements OnInit {
     return parseInt(value, 10);
   }
 }
+
 
