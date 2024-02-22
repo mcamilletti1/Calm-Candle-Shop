21ae895bd0a0f4da1724058e6f6d4f6ebf341f78
Margaret Camilletti <125098092+mcamilletti1@users.noreply.github.com>
2024-02-21 21:47:49 -0500

    created profile page

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { CheckoutPageComponent } from './components/pages/checkout-page/checkout
 import { AuthGuard } from './auth/guards/auth.guard';
 import { OrderTrackPageComponent } from './components/pages/order-track-page/order-track-page.component';
 import { PaymentPageComponent } from './components/pages/payment-page/payment-page.component';
+import { ProfilePageComponent } from './components/pages/profile-page/profile-page.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
@@ -21,6 +22,7 @@ const routes: Routes = [
   {path: 'checkout', component: CheckoutPageComponent, canActivate:[AuthGuard]},
   {path: 'payment', component: PaymentPageComponent, canActivate:[AuthGuard]},
   {path: 'track/:orderId', component: OrderTrackPageComponent, canActivate:[AuthGuard]},
+  {path: 'profile', component: ProfilePageComponent, canActivate:[AuthGuard]}
 ];
 
 @NgModule({
---
+++ frontend/src/app/components/pages/profile-page/profile-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/shared/models/User';
+
+@Component({
+    selector: 'app-profile-page',
+    templateUrl: './profile-page.component.html',
+    styleUrls: ['./profile-page.component.css']
+})
+export class ProfilePageComponent implements OnInit {
+    user: User;
+
+    constructor(private userService: UserService) { }
+
+    ngOnInit(): void {
+        this.userService.userObservable.subscribe((user: User) => {
+            this.user = user;
+        })
+    }
+}
