b51288005f479f21773fbd3d09224da15072d6e2
Margaret Camilletti <125098092+mcamilletti1@users.noreply.github.com>
2024-03-04 14:37:11 -0500

    added udate profile pic feature

--- frontend/src/app/components/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/components/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { User } from 'src/app/shared/models/User';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile-page',
@@ -11,7 +13,7 @@ export class ProfilePageComponent implements OnInit {
     
   user!:User;
   constructor( 
-    private userService: UserService) {
+    private userService: UserService, private http: HttpClient) {
         userService.userObservable.subscribe((newUser) => {
             this.user = newUser;
           })
@@ -24,5 +26,28 @@ export class ProfilePageComponent implements OnInit {
 get isAuth(){
     return this.user.token;
   }
+
+  onFileSelected(event: any): void {
+    const file: File = event.target.files[0];
+    if (file) {
+        this.uploadFile(file);
+    }
+ }
+
+ uploadFile(file: File): void {
+    const formData = new FormData();
+    formData.append('profilePic', file);
+
+    this.http.post(`${environment.apiUrl}/upload-profile-pic`, formData).subscribe({
+        next: (response: any) => {
+            this.user.profilePic = response.profilePic;
+
+            this.userService.updateUserObservable(this.user);
+        },
+        error: (err) => console.error(err)
+    })
+ }
+
+
 
 }
